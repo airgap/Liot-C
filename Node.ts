0023d14e20bc562d3f68a3e0c1c7bf5d5919d135
airgap <nick.muzzin@gmail.com>
2019-10-20 05:25:43 -0400

    Decorative changes

--- Node.ts
+++ Node.ts
@@ -1,9 +1,9 @@
 import {Network} from "./Network";
-import {IngestNode} from "./IngestNode";
 
 export class Node {
     uuid = () => {
         let str = '', alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', c = 0;
+        // Needs simplifying. Golfing this code was not an ideal approach
         for(;c<25;) str += alph[~~(Math.random()*25)]+((4-c++)%5||c>23?'':'-');
         return str;
     };
@@ -12,6 +12,6 @@ export class Node {
     toString = () => this.id;
     network: Network | null = null;
     send: (value: any) => boolean = (value: any) => {
-        return this.network ? this.network.send(this, value) : !1;
+        return this.network ? this.network.send(this, value) : false;
     };
 }
